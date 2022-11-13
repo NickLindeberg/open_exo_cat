1d170914273142e50bf2dfd0c80066e94f063f3b
Nick Lindeberg <lindebergnick@gmail.com>
2022-11-13 16:29:54 -0700

    Add method for s m l planet

--- lib/exoplanet.rb
+++ lib/exoplanet.rb
@@ -1,8 +1,23 @@
 class Exoplanet
-  attr_reader :planet_identifer, :star_temp
+  attr_reader :planet_identifer,
+              :star_temp,
+              :size,
+              :discovery_year
 
   def initialize(data)
     @planet_identifer = data["PlanetIdentifier"]
     @star_temp = data["HostStarTempK"] == "" ? 0 : data["HostStarTempK"]
+    @size = data["RadiusJpt"]
+    @discovery_year = data["DiscoveryYear"]
+  end
+
+  def tshirt_size
+    if @size < 1
+      "small"
+    elsif @size < 2
+      "medium"
+    else
+      "large"
+    end
   end
 end
--- spec/exoplanet_spec.rb
+++ spec/exoplanet_spec.rb
@@ -2,10 +2,22 @@ require './lib/exoplanet.rb'
 require 'pry'
 
 RSpec.describe Exoplanet do
-  let(:planet_data) { {"PlanetIdentifier"=>"Rad Planet", "HostStarTempK"=>1290 } }
+  let(:planet_data) { {"PlanetIdentifier"=>"Rad Planet",
+                       "HostStarTempK"=>1290,
+                       "DiscoveryYear"=>2012,
+                       "RadiusJpt"=>0.054 } }
+  let(:orphan_planet_data) { {"PlanetIdentifier"=>"Lonely Planet",
+                              "HostStarTempK"=>"",
+                              "DiscoveryYear"=>2019,
+                              "RadiusJpt"=>1.9 } }
+  let(:large_planet_data) { {"PlanetIdentifier"=>"Grande Planet",
+                              "HostStarTempK"=>2900,
+                              "DiscoveryYear"=>1986,
+                              "RadiusJpt"=>2.9 } }
+
   let(:planet) { Exoplanet.new(planet_data)}
-  let(:orphan_planet_data) { {"PlanetIdentifier"=>"Lonely Planet", "HostStarTempK"=>""} }
   let(:orphan_planet) { Exoplanet.new(orphan_planet_data) }
+  let(:large_planet) { Exoplanet.new(large_planet_data)}
 
   it 'exists' do
     expect(planet).to be_instance_of(Exoplanet)
@@ -14,10 +26,20 @@ RSpec.describe Exoplanet do
   it 'has attributes' do
     expect(planet.planet_identifer).to eq("Rad Planet")
     expect(planet.star_temp).to eq(1290)
+    expect(planet.discovery_year).to eq(2012)
+    expect(planet.size).to eq(0.054)
   end
 
   it 'can create orphan planet' do
     expect(orphan_planet.planet_identifer).to eq("Lonely Planet")
     expect(orphan_planet.star_temp).to eq(0)
+    expect(orphan_planet.discovery_year).to eq(2019)
+    expect(orphan_planet.size).to eq(1.9)
+  end
+
+  it 'can assign small, medium or large to planet' do
+    expect(planet.tshirt_size).to eq("small")
+    expect(orphan_planet.tshirt_size).to eq("medium")
+    expect(large_planet.tshirt_size).to eq("large")
   end
 end
