e19eadebe87b2a9dafab0d54d4cc2fdeb4ea8c42
Nick Lindeberg <lindebergnick@gmail.com>
2022-11-14 10:52:47 -0700

    Update service spec to use mock data instead of calling endpoint for spec runs

--- spec/exoplanet_service_spec.rb
+++ spec/exoplanet_service_spec.rb
@@ -4,6 +4,34 @@ require 'pry'
 RSpec.describe ExoplanetService do
   let(:service)      { described_class.new }
   let(:planet_array) { service.collect_planet_data }
+  let(:mock_data)    { {"PlanetIdentifier"=>"KOI-1843.03",
+                        "TypeFlag"=>0,
+                        "PlanetaryMassJpt"=>0.0014,
+                        "RadiusJpt"=>0.054,
+                        "PeriodDays"=>0.1768913,
+                        "SemiMajorAxisAU"=>0.0048,
+                        "Eccentricity"=>"",
+                        "PeriastronDeg"=>"",
+                        "LongitudeDeg"=>"",
+                        "AscendingNodeDeg"=>"",
+                        "InclinationDeg"=>72,
+                        "SurfaceTempK"=>"",
+                        "AgeGyr"=>"",
+                        "DiscoveryMethod"=>"transit",
+                        "DiscoveryYear"=>2012,
+                        "LastUpdated"=>"13/07/15",
+                        "RightAscension"=>"19 00 03.14",
+                        "Declination"=>"+40 13 14.7",
+                        "DistFromSunParsec"=>"",
+                        "HostStarMassSlrMass"=>0.46,
+                        "HostStarRadiusSlrRad"=>0.45,
+                        "HostStarMetallicity"=>0,
+                        "HostStarTempK"=>3584,
+                        "HostStarAgeGyr"=>""} }
+
+  before(:each) do
+    allow_any_instance_of(described_class).to receive(:collect_planet_data).and_return([mock_data])
+  end
 
   it 'exists' do
     expect(service).to be_instance_of(ExoplanetService)
@@ -14,7 +42,7 @@ RSpec.describe ExoplanetService do
   end
 
   it 'can get the correct count of data from endpoint' do
-    expect(planet_array.count).to eq(3426)
+    expect(planet_array.count).to eq(1)
   end
 
   it 'has data planet_identifer' do
